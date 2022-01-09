f330b4ee4934000af70e42f562bf6186ee73b6f9
zhangli <iszhangli@126.com>
2022-01-09 23:26:53 +0800

    update time series

---
+++ TimeSeries/tools/processing.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/1/7 19:22
+# @Author : li.zhang
+# @File : processing.py
+
+from tools.pyp import *
+
+def average_smoothing(signal, kernel_size=3, stride=1):
+    sample = []
+    start = 0
+    end = kernel_size
+
+    while end < len(signal):
+        start += stride
+        end += stride
+        me = np.mean(signal[start:end])
+        re = np.ones(end-start)*me
+        sample.extend(re)
+
+    return np.array(sample)
+
+
+single = np.array([2,3,4,6,7,3,8,6,4,7,4,3,8,9,6])
+print(average_smoothing(single))
--- TimeSeries/tools/pyp.py
+++ TimeSeries/tools/pyp.py
@@ -2,3 +2,5 @@
 # @Time : 2022/1/5 16:56
 # @Author : li.zhang
 # @File : pyp.py
+
+import numpy as np
