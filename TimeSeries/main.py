e34895a904b700860f6735a32519a69582a1c884
zhangli <iszhangli@126.com>
2022-02-15 21:57:13 +0800

    20220215

---
+++ TimeSeries/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/2/10 10:37
+# @Author : li.zhang
+# @File : main.py
+
+
+def returnNull(x, flag=''):
+    if x is None:
+        if flag == '1':
+            return
+        else:
+            return
+    return
+
+r = returnNull(None, '')
+print(r==None)
